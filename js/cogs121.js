167faff2735ac9b138acb6e290eeb4c83d51908f
Shiny01 <29282283@qq.com>
2017-04-24 14:34:59 -0800

    Ruoqi Liu: Added Leaflet

---
+++ js/cogs121.js
@@ -0,0 +1,3 @@
+/**
+ * Created by LRQ on 2017/4/23.
+ */
---
+++ js/map.js
@@ -0,0 +1,18 @@
+/**
+ * Created by LRQ on 2017/4/23.
+ */
+
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+}).addTo(mymap);
+
+function showMark() {
+    L.marker([51.5, -0.09]).addTo(mymap)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+}
