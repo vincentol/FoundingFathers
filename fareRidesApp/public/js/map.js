5bb824406d21ee660e16b1e1e2d124e746eb0d28
VincentOL <vincentohwell@gmail.com>
2017-04-24 16:28:56 -0700

    adjusted zoom

--- fareRidesApp/public/js/map.js
+++ fareRidesApp/public/js/map.js
@@ -2,7 +2,7 @@
  * Created by LRQ on 2017/4/23.
  */
 
-var mymap = L.map('map').setView([32.878486, -117.241435], 18);
+var mymap = L.map('map').setView([32.878486, -117.241435], 15);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
