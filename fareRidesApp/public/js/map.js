a24277e02f3506359021a392b51ebd68517a198a
VincentOL <vincentohwell@gmail.com>
2017-04-24 16:26:57 -0700

    centered map to room

--- fareRidesApp/public/js/map.js
+++ fareRidesApp/public/js/map.js
@@ -2,7 +2,7 @@
  * Created by LRQ on 2017/4/23.
  */
 
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([32.878486, -117.241435], 18);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -11,8 +11,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 }).addTo(mymap);
 
-function showMark() {
-    L.marker([51.5, -0.09]).addTo(mymap)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+window.onload = function showMark() {
+    L.marker([32.878486, -117.241435]).addTo(mymap)
+        .bindPopup('Current Location')
         .openPopup();
 }
