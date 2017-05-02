c93deb3ebcbc577bf611d919a12057d6c46d055e
VincentOL <vincentohwell@gmail.com>
2017-05-01 18:14:19 -0700

    Now added sidebar

--- public/js/cogs121.js
+++ public/js/cogs121.js
@@ -1,3 +1,11 @@
 /**
  * Created by LRQ on 2017/4/23.
  */
+
+function openNav() {
+  document.getElementById("mymenu").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mymenu").style.width = "0";
+}
